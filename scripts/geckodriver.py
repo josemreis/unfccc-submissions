a1c65a4973cf4887001be22b38bca4d9ec5a8bba
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 15:01:30 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/geckodriver.py
+++ scripts/geckodriver.py
@@ -41,7 +41,26 @@ def main() -> None:
     if needs_geckodriver:
         download_geckodriver()
         with tarfile.open(geckodriver_path) as my_tarfile:
-            my_tarfile.extractall("resources")
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(my_tarfile, "resources")
             my_tarfile.close
         os.remove(geckodriver_path)
 
